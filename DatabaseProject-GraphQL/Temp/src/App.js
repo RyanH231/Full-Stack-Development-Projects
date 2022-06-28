ac824fcd0e1334a38ee526c2ab77fa505e018e90
Ryan Hodges <ryanhodges47@gmail.com>
2022-06-28 09:45:07 -0400

    Book Database - GraphQL
    A GraphQL project used to create and store book titles and author names. The database is implemented through Mongoose and Mongo DB. The temp files contains React files for the project, implementing the Apollo Client to reach into GraphQL and the server side. Construction of this side is still in progress.

---
+++ DatabaseProject-GraphQL/Temp/src/App.js
@@ -0,0 +1,27 @@
+
+import ApolloClient from "apollo-boost";
+import {ApolloProvider} from "react-apollo";
+
+//Components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+
+const client = new ApolloClient(
+  {
+    uri: "http://localhost:4000/graphql"
+  }
+)
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="main">
+        <h1> Reading List</h1>
+        <BookList />
+        <AddBook />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
